f0e64c8fdf13fb200ddf1adfd696dfd23b381e6d
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-06-03 05:21:35 +0700

    merge prepare

--- src/objects/canvas.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CANVAS_HPP
-#define CANVAS_HPP
-
-#include <iostream>
-#include "../util/wo2glib.hpp"
-
-// NOT YET IMPLEMENTED
-
-#endif
--- src/util/wo2util.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef WO2UTIL_HPP
-#define WO2UTIL_HPP
-
-#define TO_FPS(FPS_VALUE) 1000/FPS_VALUE
-#define randint(min, max) std::rand() % max + min
-
-#endif
